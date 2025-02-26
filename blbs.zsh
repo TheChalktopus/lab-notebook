bf5c91d1ba1f7f827d8a17738c9cdd3b100eac81
Abby Kinney <ak@ACK-MacBook-Pro.local>
2025-02-25 22:41:59 -0600

    building the site again

--- blbs.zsh
+++ blbs.zsh
@@ -2,7 +2,7 @@
 
 emacs -Q --script ~/Sites/lab-notebook/build-site.el
 cd ~/Sites/lab-notebook/
-git pull origin main
+git pull
 git add -A
 git commit -m "building the site again"
 git push origin main
