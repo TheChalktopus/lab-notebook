05067c18bd82ec62c2d538f8bb8fc5e5cab0d680
Abby Kinney <ak@ACK-MacBook-Pro.local>
2025-02-26 09:47:29 -0600

    building the site again

--- build-site.el
+++ build-site.el
@@ -16,27 +16,7 @@
 ;; Load the publishing system
 (require 'ox-publish)
 
-;; Customize the HTML output
-;(setq org-html-validation-link nil            ;; Don't show validation link
-;      org-html-head-include-scripts nil       ;; Use our own scripts
-;      org-html-head-include-default-style nil ;; Use our own styles
-;      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
 
-;; Define the publishing project
-(setq org-publish-project-alist
-      (list
-       (list "org-site:main"
-             :recursive t
-             :base-directory "~/roam/lab-files-org/"
-             :publishing-function 'org-html-publish-to-html
-             :publishing-directory "~/Sites/lab-notebook/docs/"
-	     :body-only t
-	     :with-toc nil)           
-       ))
-(setq org-export-with-section-numbers nil)
-;; Generate the site output
-(org-store-agenda-views)
-(org-publish-all t)
 (add-to-list 'org-agenda-files "~/orgs/obligations.org" )
 (add-to-list 'org-agenda-files "~/orgs/" )
 (add-to-list 'org-agenda-files "~/lab-files/lab-journal/")
@@ -61,6 +41,29 @@
          nil
          ("~/roam/lab-files-org/todo.org")))
 	  )
+(org-store-agenda-views)
+
+;; Customize the HTML output
+;(setq org-html-validation-link nil            ;; Don't show validation link
+;      org-html-head-include-scripts nil       ;; Use our own scripts
+;      org-html-head-include-default-style nil ;; Use our own styles
+;      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+
+;; Define the publishing project
+(setq org-publish-project-alist
+      (list
+       (list "org-site:main"
+             :recursive t
+             :base-directory "~/roam/lab-files-org/"
+             :publishing-function 'org-html-publish-to-html
+             :publishing-directory "~/Sites/lab-notebook/docs/"
+	     :body-only t
+	     :with-toc nil)           
+       ))
+(setq org-export-with-section-numbers nil)
+;; Generate the site output
+
+(org-publish-all t)
 
 
 
