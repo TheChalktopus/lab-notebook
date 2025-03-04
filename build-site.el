a17285fe80a2898ba35762aa366816d07ac6eadf
Abby Kinney <ak@ACK-MacBook-Pro.local>
2025-03-03 19:26:31 -0600

    building the site again

--- build-site.el
+++ build-site.el
@@ -38,11 +38,10 @@
 	  '(("d" "Lab Tasks"
          (
           (tags-todo "lab"  ((org-agenda-sorting-strategy '(priority-down)))))
-         nil
+         ((org-agenda-remove-tags t))
          ("~/roam/lab-files-org/todo.org")))
 	  )
-(org-store-agenda-views)
-
+(org-batch-store-agenda-views)
 ;; Customize the HTML output
 ;(setq org-html-validation-link nil            ;; Don't show validation link
 ;      org-html-head-include-scripts nil       ;; Use our own scripts
