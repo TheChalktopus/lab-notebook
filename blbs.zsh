03a3b4b78a8fd9594f620c567cb7ef0bc2797507
Abby Kinney <ak@ACK-MacBook-Pro.local>
2025-03-10 01:11:59 -0500

    building the site again

--- blbs.zsh
+++ blbs.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 emacs -Q --script ~/Sites/lab-notebook/build-site.el
 cd ~/Sites/lab-notebook/
-python3.13 -i tag_generator.py
+python3.13 tag_generator.py
 git pull
 git add -A
 git commit -m "building the site again"
