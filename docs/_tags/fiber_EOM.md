2f1febe65b33419475f298eef1792dc38f92095a
Abby Kinney <ak@ACK-MacBook-Pro.local>
2025-03-10 02:02:43 -0500

    building the site again

--- docs/_tags/fiber_EOM.md
+++ docs/_tags/fiber_EOM.md
@@ -28,3 +28,9 @@ title: "Tag: fiber EOM"
 tag: fiber EOM
 robots: noindex
 ---
+---
+layout: tagpage
+title: "Tag: fiber EOM"
+tag: fiber EOM
+robots: noindex
+---
--- todo_generator.py
+++ todo_generator.py
@@ -13,13 +13,13 @@ import os
 projects_dir = '/Users/ak/roam/lab-files-org/_projects/'
 tag_dir = './docs/_tags/'
 
-filenames = glob.glob('/Users/ak/roam/lab-files-org/todo.org')
+filename = glob.glob('/Users/ak/roam/lab-files-org/todo.org')
 f = open(filename, 'r')
 todos = [line for line in f]
 f.close()
 
-todo_filename = 'todo.md'
-f = open(tag_filename, 'a')
+todo_filename = './docs/todo.md'
+f = open(todo_filename, 'a')
 write_str = '---\nlayout: tagpage\ntitle: \"Tag: ' + tag + '\"\ntag: [tasks]' + tag + '\nrobots: noindex\n---\n\n\n'
 for line in todos :
     write_str.append(line+"\n")
